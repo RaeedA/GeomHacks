e5429c1b4bc7193da6b069e17a11acbcff3bd28a
RaeedA <raeedazom@icloud.com>
2020-06-14 15:47:03 -0700

    starting api

--- GroSpace/src/main/java/src/GroSpace/GroSpaceApplication.java
+++ GroSpace/src/main/java/src/GroSpace/GroSpaceApplication.java
@@ -2,7 +2,11 @@ package src.GroSpace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class GroSpaceApplication {
 
@@ -10,5 +14,9 @@ public class GroSpaceApplication {
 		SpringApplication.run(GroSpaceApplication.class, args);
 	}
 	
-
+	@GetMapping("/validate")
+	public boolean validatePlant(@RequestParam(value = "plant", defaultValue = "") String plant)
+	{
+	    return false;
+	}
 }

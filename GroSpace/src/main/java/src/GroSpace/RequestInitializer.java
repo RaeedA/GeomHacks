1c5ba73a3188e9be03ffb6c3b9ae8f5c5dba39df
Luca <electroqobra@gmail.com>
2020-06-14 22:20:46 -0400

    Add Content

--- GroSpace/src/main/java/src/GroSpace/RequestInitializer.java
+++ GroSpace/src/main/java/src/GroSpace/RequestInitializer.java
@@ -1,5 +1,12 @@
 package src.GroSpace;
 
-public class RequestInitializer {
+import org.json.simple.JSONObject;
 
+public class RequestInitializer {
+    Request r;
+    public RequestInitializer(int length, int width, int height) {
+        Request r = new Request();
+        r.addData(height, width, length, 2, 4, 3);
+        JSONObject APIResponse = r.sendRequest();
+    }
 }

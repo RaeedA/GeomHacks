0025b8d997c9a4d1987039ab4f0ac9bf76a7f09e
RaeedA <raeedazom@icloud.com>
2020-06-14 17:30:06 -0700

    website moving

--- GroSpace/src/main/java/src/website/intropage.js
+++
@@ -1,4 +0,0 @@
-let padding = function() {document.getElementById("title").style.paddingTop = window.innerHeight/16+"px"};
-padding();
-window.onresize = padding;
-document.getElementById("started").onclick = function() {window.location.href="formpage.html"};
